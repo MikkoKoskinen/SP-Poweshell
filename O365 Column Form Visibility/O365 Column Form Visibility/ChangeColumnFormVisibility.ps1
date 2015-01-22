08807704151ed145f01b173cc008b100ebaf3f94
MikkoKoskinen <mikko.koskinen@outlook.com>
2015-01-22 13:25:19 -0500

    Ready sample of how to chage field visibility on list form with poweshell.

--- O365 Column Form Visibility/O365 Column Form Visibility/ChangeColumnFormVisibility.ps1
+++ O365 Column Form Visibility/O365 Column Form Visibility/ChangeColumnFormVisibility.ps1
@@ -2,19 +2,89 @@
 # This PowerShell script will demonstrate how to you can chage the visibilty of SharePoint list column in different forms like DisplayForm
 #
 
-$credential = Get-Credential
+#Parameters
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$False,Position=0)]
+	[string]$Url,
 
-Import-Module MsOnline
-Connect-MsolService -Credential $credential
+	[Parameter(Mandatory=$False,Position=1)]
+	[string]$ListName,
+
+	[Parameter(Mandatory=$False,Position=2)]
+	[string]$FieldName,
+
+	[Parameter(Mandatory=$False,Position=3)]
+	[ValidateSet("EditForm", "DisplayForm", "NewForm")]
+	[string]$ListView,
+
+	[Parameter(Mandatory=$False,Position=4)]
+	[bool]$ShowInForm
+)
 
 Import-Module Microsoft.Online.SharePoint.PowerShell
 
-Connect-SPOService -Url https://litwareinc-admin.sharepoint.com -credential $credential
+#Get SharePoint.Cliend dll:s. The path here may need to change if you used e.g. C:\Lib.. 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
 
+try
+{
+	# connect/authenticate to SharePoint Online and get ClientContext object..
+	$credential = Get-Credential
+	$clientContext = New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+	$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($credential.UserName, $credential.Password)
+	$clientContext.Credentials = $credentials
 
+	if (!$clientContext.ServerObjectIsNull.Value)
+	{ 
+		#This gets Microsoft.SharePoint.Client.List
+		$oList = $clientContext.Web.Lists.GetByTitle($ListName); 
+		#This gets Microsoft.SharePoint.Client.FieldCollection
+		$fieldColl = $oList.Fields;
 
-Disconnect-SPOService
-
+		$clientContext.Load($fieldColl);
+		$clientContext.ExecuteQuery();
+		
+		$field = $fieldColl.GetByInternalNameOrTitle($FieldName);
+		
+		$clientContext.Load($field);
+		$clientContext.ExecuteQuery();
+		
+		switch ($listView) {
+			"DisplayForm"
+			{
+				$field.SetShowInDisplayForm($ShowInForm);				
+				$clientContext.ExecuteQuery();
+				
+				$message = "Fields '" + $field.Title + "' DisplayForm visibility changed to: " + $ShowInForm
+				write-Host $message -ForegroundColor Green
+				break
+			}
+			"NewForm"
+			{
+				$field.SetShowInNewForm($ShowInForm);				
+				$clientContext.ExecuteQuery();
+				
+				$message = "Fields '" + $field.Title + "' NewForm visibility changed to: " + $ShowInForm
+				write-Host $message -ForegroundColor Green
+				break
+			}
+			"EditForm"
+			{
+				$field.SetShowInEditForm($ShowInForm);				
+				$clientContext.ExecuteQuery();
+				
+				$message = "Fields '" + $field.Title + "' EditForm visibility changed to: " + $ShowInForm
+				write-Host $message -ForegroundColor Green
+				break
+			}
+		}
+	}
+}
+finally {
+	$clientContext.Dispose()
+}
 
 #The MIT License (MIT)
 
